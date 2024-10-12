fefcaab4326a3baee2268cda1776a0ba55754b92
sugarandgugu <95059219+sugarandgugu@users.noreply.github.com>
2024-10-12 13:37:56 +0800

    Add files via upload

--- pepler_utils/utils.py
+++ pepler_utils/utils.py
@@ -1,1 +1,102 @@
+import os
+import sys
+import re
+import math
+import torch
+import random
+import pickle
+import datetime
+from .rouge import rouge
+from .bleu import compute_bleu
 
+
+def rouge_score(references, generated):
+    """both are a list of strings"""
+    score = rouge(generated, references)
+    rouge_s = {k: (v * 100) for (k, v) in score.items()}
+    '''
+    "rouge_1/f_score": rouge_1_f,
+    "rouge_1/r_score": rouge_1_r,
+    "rouge_1/p_score": rouge_1_p,
+    "rouge_2/f_score": rouge_2_f,
+    "rouge_2/r_score": rouge_2_r,
+    "rouge_2/p_score": rouge_2_p,
+    "rouge_l/f_score": rouge_l_f,
+    "rouge_l/r_score": rouge_l_r,
+    "rouge_l/p_score": rouge_l_p,
+    '''
+    return rouge_s
+
+
+def bleu_score(references, generated, n_gram=4, smooth=False):
+    """a list of lists of tokens"""
+    formatted_ref = [[ref] for ref in references]
+    bleu_s, _, _, _, _, _ = compute_bleu(formatted_ref, generated, n_gram, smooth)
+    return bleu_s * 100
+
+
+def two_seq_same(sa, sb):
+    if len(sa) != len(sb):
+        return False
+    for (wa, wb) in zip(sa, sb):
+        if wa != wb:
+            return False
+    return True
+
+
+def unique_sentence_percent(sequence_batch):
+    unique_seq = []
+    for seq in sequence_batch:
+        count = 0
+        for uni_seq in unique_seq:
+            if two_seq_same(seq, uni_seq):
+                count += 1
+                break
+        if count == 0:
+            unique_seq.append(seq)
+
+    return len(unique_seq) / len(sequence_batch), len(unique_seq)
+
+
+def feature_detect(seq_batch, feature_set):
+    feature_batch = []
+    for ids in seq_batch:
+        feature_list = []
+        for i in ids:
+            if i in feature_set:
+                feature_list.append(i)
+        feature_batch.append(set(feature_list))
+    return feature_batch
+
+
+def feature_matching_ratio(feature_batch, test_feature):
+    count = 0
+    for (fea_set, fea) in zip(feature_batch, test_feature):
+        if fea in fea_set:
+            count += 1
+
+    return count / len(feature_batch)
+
+
+def feature_coverage_ratio(feature_batch, feature_set):
+    features = set()
+    for fb in feature_batch:
+        features = features | fb
+
+    return len(features) / len(feature_set)
+
+
+def feature_diversity(feature_batch):
+    list_len = len(feature_batch)
+
+    total_count = 0
+    for i, x in enumerate(feature_batch):
+        for j in range(i + 1, list_len):
+            y = feature_batch[j]
+            total_count += len(x & y)
+
+    denominator = list_len * (list_len - 1) / 2
+    return total_count / denominator
+
+def now_time():
+    return '[' + datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f') + ']: '
